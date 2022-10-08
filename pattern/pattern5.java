36ae0f2975f3ee594382fbe54db80e519ef9ae38
Ashutosh0602 <ashu00jnp@gmail.com>
2022-10-09 00:16:50 +0530

    commit 2

--- pattern/pattern5.java
+++ pattern/pattern5.java
@@ -1,10 +1,10 @@
-public class pattern4 {
+public class pattern5 {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5 - i - 1; j++) {
+            for (int j = 0; j < i; j++) {
                 System.out.print("  ");
             }
-            for (int j = 0; j < i + 1; j++) {
+            for (int j = 0; j < 5 - i; j++) {
                 System.out.print(" *");
             }
             System.out.println("");
