90b944ec6baa6cbbf286d832861285da540ad6fe
Ashutosh0602 <ashu00jnp@gmail.com>
2022-10-17 12:32:10 +0530

    commit 4

---
+++ data types/dt.java
@@ -0,0 +1,13 @@
+public class dt {
+    public static void main(String[] args) {
+        byte b = 127;
+        b = (byte) 133;
+        System.out.println(b);
+        float a = 3.23f;
+        double d = 23.73789;
+        int c;
+        c = (int) d;
+        a = (float) d;
+        System.out.println(c);
+    }
+}
