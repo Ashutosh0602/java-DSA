518d946aca98f106bd7b4bb18ed5242366fffd3d
Ashutosh0602 <ashu00jnp@gmail.com>
2023-02-14 01:45:32 +0530

    N queens

---
+++ recursion/recursion withoutArray/gfd.java
@@ -0,0 +1,17 @@
+public class gfd {
+    public static void main(String[] args) {
+        // System.out.println(myPow(2.000, -2));
+        System.out.println(9 / 4);
+    }
+
+    // public static double myPow(double x, int n) {
+    // if (n == 0) {
+    // return 1;
+    // }
+    // if (n < 0) {
+    // return (1 / x) * myPow(x, n + 1);
+    // } else {
+    // return x * myPow(x, n - 1);
+    // }
+    // }
+}
---
+++ recursion/recursion withoutArray/nQueens.java
@@ -0,0 +1,41 @@
+public class nQueens {
+    public static void main(String[] args) {
+        boolean[][] board = new boolean[4][4];
+        System.out.println(countQueens(board, 4));
+    }
+
+    public static int countQueens(boolean[][] board, int row) {
+        if (row == board.length) {
+            return 1;
+        }
+        int count = 0;
+        for (int col = 0; col < board[row].length; col++) {
+            if (isItSafe(board, row, col)) {
+                board[row][col] = true;
+                count = count + countQueens(board, row + 1);
+                board[row][col] = false;
+
+            }
+        }
+        return count;
+    }
+
+    private static boolean isItSafe(boolean[][] board, int row, int col) {
+        for (int i = row; i >= 0; i++) {
+            if (board[i][col]) {
+                return false;
+            }
+        }
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j]) {
+                return false;
+            }
+        }
+        for (int i = row, j = col; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
