1020893df02102cbcdedc06e321b9cbc2281de57
Ashutosh0602 <ashu00jnp@gmail.com>
2023-07-19 16:04:21 +0530

    linked list

---
+++ Pepcoding/linked list/basic.java
@@ -0,0 +1,14 @@
+/**
+ * basic
+ */
+public class basic {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class Node {
+        int data;
+        Node next;
+    }
+}
---
+++ Pepcoding/linked list/displayList.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+
+public class displayList {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        LinkedList list = new LinkedList();
+
+        String str = br.readLine();
+        while (str.equals("quit") == false) {
+            if (str.startsWith("addLast")) {
+                int val = Integer.parseInt(str.split(" ")[1]);
+                list.addLast(val);
+            } else if (str.startsWith("size")) {
+                System.out.println(list.size());
+            } else if (str.startsWith("display")) {
+                list.display();
+            }
+            str = br.readLine();
+        }
+
+    }
+
+    public static void testList(LinkedList list) {
+        for (Node temp = list.head; temp != null; temp = temp.next) {
+            System.out.println(temp.data);
+        }
+        System.out.println(list.size);
+
+        if (list.size > 0) {
+            System.out.println(list.tail.data);
+        }
+    }
+
+    public static class Node {
+        int data;
+        Node next;
+    }
+
+    public static class LinkedList {
+        Node head;
+        Node tail;
+        int size;
+
+        void addLast(int val) {
+
+            Node temp = new Node();
+            temp.data = val;
+            temp.next = null;
+
+            if (size == 0) {
+                head = tail = temp;
+            } else {
+                tail.next = temp;
+                tail = temp;
+            }
+
+            size++;
+        }
+
+        public int size() {
+            return size;
+        }
+
+        public void display() {
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+    }
+
+}
